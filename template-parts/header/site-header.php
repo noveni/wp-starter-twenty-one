4d9f406f38a24676d6012a483bc9a8aafc1f17a8
noveni <nico.novello@gmail.com>
2021-02-02 05:00:46 +0100

    import header

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -1,6 +1,13 @@
+<?php
+/**
+ * Displays the site header.
+ *
+ */
+?>
 <header id="site-header" class="sticky-header" role="banner">
 
 <!-- Branding -->
+<?php get_template_part( 'template-parts/header/site-branding' ); ?>
 <?php get_template_part( 'template-parts/header/site-nav' ); ?>
 
 </header>
