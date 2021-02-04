950df6a05f22e1a0ed3f54f0ed79e148c19de317
noveni <nico.novello@gmail.com>
2021-02-04 06:13:38 +0100

    correct script loading

--- classes/EcrannoirTwentyOne-Scripts.php
+++ classes/EcrannoirTwentyOne-Scripts.php
@@ -11,7 +11,7 @@ class EcranNoirTwentyOne_Scripts
         $script_asset_path = get_template_directory() . '/assets/js/' . $scriptName . '.asset.php';
         $script_asset = file_exists($script_asset_path) ? require($script_asset_path) : array('dependencies' => array(), 'version' => filemtime( $script_path ));
         
-        $handleFileName = $customHandleScriptName !== '' ?? 'ecrannoirtwentyone-' . $styleName . '-scripts';
+        $handleFileName = $customHandleScriptName !== '' ? $customHandleScriptName : 'ecrannoirtwentyone-' . $styleName . '-scripts';
         // Enqueue Scripts
         wp_enqueue_script($handleFileName, get_template_directory_uri() . '/assets/js/' . $scriptName . '.js', $script_asset['dependencies'], $script_asset['version'], true);
     }
@@ -21,7 +21,7 @@ class EcranNoirTwentyOne_Scripts
         $script_asset_path = get_template_directory() . '/assets/js/theme.asset.php';
         $script_asset = file_exists($script_asset_path) ? require($script_asset_path) : array('dependencies' => array(), 'version' => filemtime( $script_path ));
         
-        $handleFileName = $customHandleStyleName !== '' ?? 'ecrannoirtwentyone-' . $styleName . '-styles';
+        $handleFileName = $customHandleStyleName !== '' ? $customHandleStyleName : 'ecrannoirtwentyone-' . $styleName . '-styles';
         // Enqueue Style
         wp_enqueue_style($handleFileName, get_template_directory_uri() . '/assets/css/' . $styleName . '.css', array(), $script_asset['version'], 'all');
     }
