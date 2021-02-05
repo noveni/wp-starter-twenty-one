202a1472f220d1405150c250774a43a91bb4af98
noveni <nico.novello@gmail.com>
2021-02-05 04:44:51 +0100

    up assets loading

--- classes/EcrannoirTwentyOne-Icons.php
+++ classes/EcrannoirTwentyOne-Icons.php
@@ -17,12 +17,14 @@
  */
 class EcranNoirTwentyOne_Icons {
 
+
+	public static $assets_path = '/assets/img/';
     /**
      * Brand SVG
      * 
      */
     public static $brand_svg = array(
-        'logo'			=> ['path' => '/assets/img/logo.svg'],
+        'logo'			=> ['path' => 'logo.svg'],
     );
 	/**
 	 * User Interface icons – svg sources.
@@ -35,7 +37,7 @@ class EcranNoirTwentyOne_Icons {
 		'arrow_right' => '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="m4 13v-2h12l-4-4 1-2 7 7-7 7-1-2 4-4z" fill="currentColor"/></svg>',
 		'arrow_left'  => '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 13v-2H8l4-4-1-2-7 7 7 7 1-2-4-4z" fill="currentColor"/></svg>',
 		'close'       => '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 10.9394L5.53033 4.46973L4.46967 5.53039L10.9393 12.0001L4.46967 18.4697L5.53033 19.5304L12 13.0607L18.4697 19.5304L19.5303 18.4697L13.0607 12.0001L19.5303 5.53039L18.4697 4.46973L12 10.9394Z" fill="currentColor"/></svg>',
-		'menu'        => '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4.5 6H19.5V7.5H4.5V6ZM4.5 12H19.5V13.5H4.5V12ZM19.5 18H4.5V19.5H19.5V18Z" fill="currentColor"/></svg>',
+		'menu'        => [ 'path' => 'menu.svg' ],
 		'plus'        => '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M18 11.2h-5.2V6h-1.6v5.2H6v1.6h5.2V18h1.6v-5.2H18z" fill="currentColor"/></svg>',
 		'minus'       => '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6 11h12v2H6z" fill="currentColor"/></svg>',
 	);
@@ -180,13 +182,13 @@ class EcranNoirTwentyOne_Icons {
             $icon_content = $arr[ $icon ];
             if (is_array($arr[ $icon ])) {
                 if (array_key_exists('path', $arr[$icon])) {
-                    $icon_content = file_get_contents( get_template_directory() . $arr[$icon]['path'], true);
+                    $icon_content = file_get_contents( get_template_directory() . self::$assets_path .  $arr[$icon]['path'], true);
                 }
 			}
 			if ($size) {
 				$repl = sprintf( '<svg class="svg-icon" width="%d" height="%d" aria-hidden="true" role="img" focusable="false" ', $size, $size );
 			} else {
-				$repl = sprintf( '<svg class="svg-icon" aria-hidden="true" role="img" focusable="false" ', $size, $size );
+				$repl = '<svg class="svg-icon" aria-hidden="true" role="img" focusable="false" ';
 			}
 
 			$svg = preg_replace( '/^<svg /', $repl, trim( $icon_content ) ); // Add extra attributes to SVG code.
--- src/scripts/theme/assets.js
+++ src/scripts/theme/assets.js
@@ -1,1 +1,8 @@
+
+function importAll (r) {
+  r.keys().forEach(r);
+}
+
+importAll(require.context('../../img/icons', false, /\.svg$/));
 import '../../img/logo-theme-author.svg';
+import '../../img/logo.svg';
